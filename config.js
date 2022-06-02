9f72cc60d137106c349e409cb306cb5ee3ed63b5
LiNKe9 <106754465+LiNKe9@users.noreply.github.com>
2022-06-02 21:21:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Chào công trúa Ngoc ngốc",
     introTitle: 'Helu ck Ngọc',
-    introDesc: `Sao vào đây zậy
+    introDesc: `Trào ck đã đến đây
     Nhớ tui chứ gì tui biết mà =))
     Trả lời câu hỏi ii`,
     btnIntro: 'Gét gô',
-    title: 'Phải chăng Ck và Vk là 1 nhà =)))',
+    title: 'Phải chăng Ck không bị do đường, trời không mưa thằng ml kia không say là được đi tráy phố ròi',
     desc: 'Phải chăng Ck đi ăn mì cay hải sản với toi ',
     btnYes: 'Nhớ vk lắm :3',
     btnNo: 'Không nha chêeeee :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho Tlinh nho hohoo =))',
-    reply: 'Mai 7h đón em, 2 mình đi ăn đi cháy phốooo',
+    reply: 'Mai 7h đón em, 2 mình đi ăn đi cháy phốoo',
     mess: 'Anh biết mà xăng đổ đầy bình rồi, :)) mai cháy hết cái Ngãi Giao',
     messDesc: 'Tối mai 7h, anh qua đón đi ăn nhá, đi tráy phố nhá',
     btnAccept: 'Okiiiii lun gét gô <3',
