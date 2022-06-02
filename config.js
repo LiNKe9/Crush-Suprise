3622bdbf9671d2d4584a687ff783c0fb3f114e3c
LiNKe9 <106754465+LiNKe9@users.noreply.github.com>
2022-06-02 21:27:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     Nhớ tui chứ gì tui biết mà =))
     Trả lời câu hỏi ii`,
     btnIntro: 'Gét gô',
-    title: 'Phải chăng Ck không bị do đường, trời không mưa thằng ml kia không say là được đi tráy phố ròi',
-    desc: 'Phải chăng Ck đi ăn mì cay hải sản với toi ',
+    title: 'Đi trày phố thoy, mì kay mãi đỉnk',
+    desc: 'Phải chăng Ck không bị do đường, trời không mưa thằng ml kia không say là được đi tráy phố ròi ',
     btnYes: 'Đi 6 ngày 6 đêm, mẹ chửi hong về, anh gọi không về, Gét gô',
     btnNo: 'Không nha chêeeee :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
