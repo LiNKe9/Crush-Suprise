433335a8e8bbaeaf6e7917eed70e6e1b2b133934
LiNKe9 <106754465+LiNKe9@users.noreply.github.com>
2023-04-19 16:41:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     titleWeb: "Chào công trúa Ngoc ngốc",
-    introTitle: 'Helu ck Ngọc',
+    introTitle: 'Helu Bé iu cụa anhhh',
     introDesc: `Trào ck đã đến đây
     Nhớ tui chứ gì tui biết mà =))
     Trả lời câu hỏi ii`,
     btnIntro: 'Gét gô',
-    title: 'Đi trày phố thoy, mì kay mãi đỉnk',
-    desc: 'Thi xong có thưởng nhe bà ck',
+    title: 'Tháng 10 về với ahhhhh',
+    desc: 'Bé làm vợ anh nhóaaaaaaaaa',
     btnYes: 'Đi 6 ngày 6 đêm, mẹ chửi hong về, anh gọi không về, Gét gô',
     btnNo: 'Không nha chêeeee :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho Tlinh nho hohoo =))',
-    reply: 'Mai 7h đón em, 2 mình đi ăn đi cháy phốoo',
-    mess: 'Anh biết mà xăng đổ đầy bình rồi, :)) mai cháy hết cái Ngãi Giao',
-    messDesc: 'Tối mai 7h, anh qua đón đi ăn nhá, đi tráy phố nhá',
+    reply: 'Tháng 10 anh với em cháy phố hết cái hà nội nha',
+    mess: 'Anh biết mà xăng đổ đầy bình rồi, :)) cháy phố hà nội thôi',
+    messDesc: 'Xong về lại VT cháy sập cái VT nhá',
     btnAccept: 'Okiiiii lun gét gô <3',
     messLink: 'https://www.facebook.com/messages/t/100040770521015' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
